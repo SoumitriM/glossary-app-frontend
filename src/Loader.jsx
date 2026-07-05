5868a31bff39adc2a0a1c3af543a6be66c9f7de2
Soumitri Mukherjee <soumitri@Soumitris-MacBook-Air.local>
2026-07-05 15:16:34 +0200

    Temp: include dist for subtree initialization

--- src/Loader.jsx
+++
@@ -1,8 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center h-[40rem]">
-    <span className="loading loading-spinner text-primary"></span>
-  </div>
-  )
-}
- export default Loader;
